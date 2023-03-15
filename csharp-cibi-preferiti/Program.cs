974ba099de2946883db9d6e89503bda53e57bd78
AdrianBottini <adrianboh@gmail.com>
2023-03-15 15:34:46 +0100

    Cibi Preferiti

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
+string[] favouriteFood = { "Kebab", "Pizza", "Pasta", "Frutta", "Riso" };
+Console.WriteLine(favouriteFood.Length);
+
+for ( int i = 0; i < favouriteFood.Length; i++)
+{
+    Console.WriteLine( (i) + favouriteFood[i] );
+}
+
+Console.WriteLine("In poolposition c'è:" + favouriteFood[1]);
+
+Console.WriteLine("In fondo alla classifica c'è:" + favouriteFood[4]);
+
+Console.WriteLine("A metà della classifica c'è:" + favouriteFood[favouriteFood.Length / 2]);
 
 
